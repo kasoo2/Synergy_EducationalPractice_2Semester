fff5fc89f0eb272ae708adfaed1d99baebd63f93
Никита Трубин <niktrubin397sp@mail.ru>
2025-07-10 18:15:12 +0300

    Инициализация проекта "каталог книг" для кейс-задачи №4.

---
+++ TaskCase4/BookCatalogue.dpr
@@ -0,0 +1,23 @@
+library BookCatalogue;
+
+uses
+  Winapi.ActiveX,
+  System.Win.ComObj,
+  Web.WebBroker,
+  Web.Win.ISAPIApp,
+  Web.Win.ISAPIThreadPool,
+  WebModuleUnit in 'WebModuleUnit.pas' {WebModule2: TWebModule};
+
+{$R *.res}
+
+exports
+  GetExtensionVersion,
+  HttpExtensionProc,
+  TerminateExtension;
+
+begin
+  CoInitFlags := COINIT_MULTITHREADED;
+  Application.Initialize;
+  Application.WebModuleClass := WebModuleClass;
+  Application.Run;
+end.
